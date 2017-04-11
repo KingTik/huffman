71cd5f7fb807ed3bba927fc54d0fa65a986869d3
tomek wesolowski <tik.wesolowski@wp.pl>
2017-04-11 22:26:18 +0200

    ADD: huffman and tree

---
+++ huffman.py
@@ -0,0 +1,34 @@
+import tree.py
+
+
+text = "Pok pok disrupt austin prism, skateboard pug wolf echo park. XOXO twee cornhole YOLO. Disrupt asymmetrical tbh fixie. Cronut tumeric vinyl polaroid, shoreditch artisan selvage tote bag fap iceland pug thundercats art party drinking vinegar. Sriracha distillery leggings intelligentsia freegan meh. Intelligentsia bitters put a bird on it locavore, next level beard irony whatever ethical. Hashtag chia air plant, green juice mustache prism taxidermy pork belly lumbersexual bushwick try-hard. Mixtape af pickled, sartorial hashtag biodiesel heirloom occupy readymade roof party green juice thundercats. Pinterest everyday carry leggings, kale chips post-ironic pok pok semiotics echo park enamel pin activated charcoal poke put a bird on it. Narwhal knausgaard cold-pressed poutine, coloring book hot chicken meh stumptown microdosing VHS swag kombucha. Hella cray jean shorts you probably haven't heard of them synth. Yuccie succulents organic brunch, portland enamel pin man bun disrupt fanny pack. Swag ramps church-key, jean shorts everyday carry art party la croix pickled iPhone listicle. Pitchfork deep v franzen, godard tbh humblebrag man bun blog poutine ennui biodiesel. Authentic raw denim slow-carb echo park four dollar toast meggings cold-pressed, godard williamsburg art party activated charcoal. Cold-pressed photo booth drinking vinegar marfa shoreditch. Echo park biodiesel gochujang fanny pack jianbing banh mi. Wolf vexillologist chartreuse brunch, seitan jean shorts shoreditch gastropub. Green juice butcher jianbing thundercats, chartreuse wolf shoreditch. Mumblecore retro disrupt, selvage messenger bag pitchfork etsy glossier literally try-hard gochujang cold-pressed 8-bit chartreuse prism. Pop-up hammock prism, raclette celiac squid small batch vice DIY chartreuse. Forage sartorial truffaut, cornhole hammock whatever paleo prism twee chambray chia humblebrag. Yuccie hoodie cornhole, vinyl af helvetica activated charcoal. Tacos cred meh 8-bit cold-pressed, iPhone chartreuse. Subway tile salvia gochujang twee listicle next level. Shabby chic keytar forage next level neutra, chartreuse salvia hell of. Ugh austin brunch asymmetrical selfies cliche forage. Brooklyn poutine la croix church-key."
+text = text.lower()
+
+
+class huffman:
+    
+    def __init__(self, text):
+        self.text = text
+        self.slownik = {}
+        self.len = len(text)
+
+    def coutnProb(self):
+        for letter in text:
+            if slownik.has_key(letter) == False:
+                slownik[letter] = 1
+            else:
+                slownik[letter] = slownik[letter] +1           
+
+
+
+
+drzewo = Tree(100, 'a')
+drzewo.insert(101,'c')
+drzewo.insert(102, 'f')
+drzewo.insert(2, 'k')
+drzewo.display()
+
+
+
+
+
---
+++ tree.py
@@ -0,0 +1,97 @@
+from copy import copy, deepcopy
+
+
+class Tree:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.right = None
+        self.left = None
+
+    def getKey(self):
+        return copy(self.key)
+
+    def getValue(self):
+        return copy(self.value)
+
+    def getRight(self):
+        return copy(self.right)
+
+    def getLeft(self):
+        return copy(self.left)
+
+    def putRight(self, key, value):
+        self.right = Tree(key, value)
+
+    def putLeft(self, key, value):
+        self.left = Tree(key, value)
+
+    def checkRight(self):
+        if self.getRight() == None:
+            return False
+        else:
+            return True
+
+    def checkLeft(self):
+        if self.getLeft() == None:
+            return False
+        else:
+            return True
+
+
+    def display(self, level=1):
+        print '-'*level + str(self.getValue())
+
+        if self.checkLeft()==True:
+            self.getLeft().display(level+1)
+        if self.checkRight()==True:
+            self.getRight().display(level+1)
+
+    def insert(self, key, value):
+        
+        if self.getKey == key:
+            return -1 #key already in tree
+        else:
+                
+            done = False
+            tmp = self
+
+            while(done == False):
+                if tmp.key > key:
+                    #to the left
+                    if tmp.checkLeft() == True:
+                        tmp = tmp.left
+                    else:
+                        tmp.putLeft(key, value)
+                        done = True
+
+                else:
+                    #to the right
+                    if tmp.checkRight()==True:
+                        tmp = tmp.right
+                    else:
+                        tmp.putRight(key, value)
+                        done=True
+
+
+
+'''
+drzewo = Tree(100, 'a')
+drzewo.insert(101,'c')
+drzewo.insert(102, 'f')
+drzewo.insert(2, 'k')
+drzewo.display()
+
+
+
+a=3
+b = copy(a)
+b = 2
+print a
+
+
+drzewo.putLeft(99,'c')
+drzewo.putRight(111,'f')
+drzewo.display()        
+'''
